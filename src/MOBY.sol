b9c86a5ea3587a966e35b6d67931421ef6c5a309
Mobydex <141993159+mobydex-labs@users.noreply.github.com>
2023-08-21 01:09:50 +0900

    Update MOBY.sol

--- src/MOBY.sol
+++ src/MOBY.sol
@@ -5,7 +5,6 @@ pragma solidity 0.8.16;
 import {ERC20} from "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MOBY is ERC20 {
-    uint256 public constant MAX_SUPPLY = 20_000_000 ether;
 
     address public masterchef;
     address public masterchefSetter;
