1d90c77bfcdd5d21dbf1bc545819a4b104fe9b4a
Mobydex <141993159+mobydex-labs@users.noreply.github.com>
2023-08-25 10:51:57 +0900

    Update IesMOBY.sol

--- src/interfaces/IesMOBY.sol
+++ src/interfaces/IesMOBY.sol
@@ -5,5 +5,5 @@ pragma solidity 0.8.16;
 import {IERC20} from "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 interface IesMOBY is IERC20 {
-    function stake(uint256 amount, address to) external returns (bool);
+    function stake(uint256 amount, address to) external;
 }
